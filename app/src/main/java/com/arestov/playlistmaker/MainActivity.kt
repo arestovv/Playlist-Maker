8fc21b30ac071c076859dc39a2a83d8e52cf96a0
arestov <y.arestov@prequelapp.com>
2025-04-19 18:33:40 +0200

    add clickListener on main screen

--- app/src/main/java/com/arestov/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/arestov/playlistmaker/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.arestov.playlistmaker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonSearch = findViewById<Button>(R.id.button_search)
+        val buttonMedia = findViewById<Button>(R.id.button_media)
+        val button_setting = findViewById<Button>(R.id.button_setting)
+
+        buttonSearch.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Search!", Toast.LENGTH_SHORT).show()
+        }
+        buttonMedia.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Media!", Toast.LENGTH_SHORT).show()
+        }
+        button_setting.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на кнопку Setting!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
