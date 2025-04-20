6b06d0c67a8044ab2f24afc330ac41724e2c7514
arestov <y.arestov@prequelapp.com>
2025-04-20 18:36:41 +0200

    added button back on settings screen

--- app/src/main/java/com/arestov/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/arestov/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,20 @@
 package com.arestov.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val buttonBack = findViewById<ImageView>(R.id.button_arrow_back)
+
+        buttonBack.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
     }
 }
