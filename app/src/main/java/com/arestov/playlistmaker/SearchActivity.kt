1af25d5506681daefaaf071ee9f52c710c1a2b19
arestov <y.arestov@prequelapp.com>
2025-04-20 18:20:18 +0200

    added media and search screen

--- app/src/main/java/com/arestov/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/arestov/playlistmaker/SearchActivity.kt
@@ -1,4 +1,11 @@
 package com.arestov.playlistmaker
 
-class SearchActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+    }
 }
