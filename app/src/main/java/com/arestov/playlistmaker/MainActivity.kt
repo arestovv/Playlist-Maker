8efb3185e26fc7a075efffa708257787ad49ef47
arestov <y.arestov@prequelapp.com>
2025-06-15 14:36:20 +0200

    sprint 12 switcher theme

--- app/src/main/java/com/arestov/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/arestov/playlistmaker/MainActivity.kt
@@ -5,28 +5,39 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
+const val PLAYLIST_MAKER_PREFERENCES = "playlist_maker_preferences"
+const val SWITCH_STATE_KEY = "switch_state_key"
+
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        //Restore choose theme from file preferences
+        val sharedPrefs = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
+        val isDark = sharedPrefs.getBoolean(SWITCH_STATE_KEY, false)
+        (applicationContext as App).switchTheme(isDark)
+
+        //Set main activity
         setContentView(R.layout.activity_main)
 
         val buttonSearch = findViewById<Button>(R.id.button_search)
         val buttonMedia = findViewById<Button>(R.id.button_media)
         val buttonSettings = findViewById<Button>(R.id.button_setting)
 
-        //Search
+        //Search clickListener
         buttonSearch.setOnClickListener {
             val displayIntent = Intent(this, SearchActivity::class.java)
             startActivity(displayIntent)
         }
 
-        //Media
+        //Media clickListener
         buttonMedia.setOnClickListener {
             val displayIntent = Intent(this, MediaActivity::class.java)
             startActivity(displayIntent)
         }
 
-        //Settings
+        //Settings clickListener
         buttonSettings.setOnClickListener {
             val displayIntent = Intent(this, SettingsActivity::class.java)
             startActivity(displayIntent)
